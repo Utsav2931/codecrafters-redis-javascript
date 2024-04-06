78bf43812ded75ab0a1e510ac5643170ffe133f5
Utsav <upatel231a@gmail.com>
2024-04-06 16:32:38 -0700

    added ping command

--- app/main.js
+++ app/main.js
@@ -6,6 +6,14 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
+  connection.on("data", (data) => {
+    const pong = "+PONG\r\n";
+		const strData = data.toString();
+    console.log("Received:", strData);
+    connection.write(pong, "utf8", () => {
+			console.log("Sent data back to client")
+		});
+  });
 });
 
 server.listen(6379, "127.0.0.1");
